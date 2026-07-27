d3ae1cd3d23249675276af39e8e7dd14a8b22db7
Bin9825 <bin9825aaa@gmail.com>
2026-07-27 14:32:12 +0900

    [feat] 파이터 제외 캐릭터 모델 최신화 -> 추후 소켓 수정
    [chore] 이벤트 데이터 최신화

---
+++ Assets/_ProtoType_Merge/ASB/Scripts/Data/Generated/ChatDB메인퀘스트DataTable.cs
@@ -0,0 +1,46 @@
+﻿// Auto Generated. Do not modify.
+// Template Signature: dict=1|Chat_ID:String:Comma|Chat_Type:String:Comma|Char_Name:String:Comma|Char_Profile:String:Comma|Message_Text:String:Comma|IMG_Res:String:Comma|Next_Chat_ID:String:Comma|Branch_Group_ID:String:Comma
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ChatDB메인퀘스트Data
+{
+    public string Chat_ID;
+    public string Chat_Type;
+    public string Char_Name;
+    public string Char_Profile;
+    public string Message_Text;
+    public string IMG_Res;
+    public string Next_Chat_ID;
+    public string Branch_Group_ID;
+}
+
+[CreateAssetMenu(fileName="ChatDB메인퀘스트DataTable", menuName="DataTable/ChatDB메인퀘스트")]
+public class ChatDB메인퀘스트DataTable : ScriptableObject, ISerializationCallbackReceiver
+{
+    public List<ChatDB메인퀘스트Data> DataList = new List<ChatDB메인퀘스트Data>();
+
+    [NonSerialized]
+    public Dictionary<string, ChatDB메인퀘스트Data> DataMap = new Dictionary<string, ChatDB메인퀘스트Data>();
+
+    public void OnBeforeSerialize() { }
+
+    public void OnAfterDeserialize()
+    {
+        DataMap.Clear();
+        foreach (var data in DataList)
+        {
+            if (!DataMap.ContainsKey(data.Chat_ID))
+            {
+                DataMap.Add(data.Chat_ID, data);
+            }
+            else
+            {
+                UnityEngine.Debug.LogWarning($"[ChatDB메인퀘스트DataTable] Duplicate key: {data.Chat_ID}");
+            }
+        }
+    }
+}
---
+++ Assets/_ProtoType_Merge/ASB/Scripts/Data/Generated/이벤트1구역DataTable.cs
@@ -0,0 +1,21 @@
+﻿// Auto Generated. Do not modify.
+// Template Signature: dict=0|Field_02:String:Comma|Field_03:String:Comma|제목:String:Comma|소개:String:Comma|비고:String:Comma
+
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class 이벤트1구역Data
+{
+    public string Field_02;
+    public string Field_03;
+    public string 제목;
+    public string 소개;
+    public string 비고;
+}
+
+[CreateAssetMenu(fileName="이벤트1구역DataTable", menuName="DataTable/이벤트1구역")]
+public class 이벤트1구역DataTable : ScriptableObject
+{
+    public List<이벤트1구역Data> DataList = new List<이벤트1구역Data>();
+}
